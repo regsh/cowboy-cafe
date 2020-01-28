1e9f657f8fd2c7da66f25c48cca619e6ea7d77a6
rshale <rshale@HFRR-3609-ACRH>
2020-01-27 19:10:59 -0600

    Added ribs class

---
+++ Data/RustlersRibs.cs
@@ -0,0 +1,26 @@
+﻿namespace CowboyCafe.Data
+{
+    class RustlersRibs
+    {
+        /// <summary>
+        /// The price of the ribs
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+        /// <summary>
+        /// Calorie content of the ribs
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+    }
+}
