3fdf6367cd465b8d967ce96eb7f460fdb811a4fe
rshale <rshale@HFRR-3609-ACRH>
2020-01-27 19:21:40 -0600

    Added pulled pork entree

---
+++ Data/PecosPork.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Class to represent the Pecos Pulled Pork entree
+    /// </summary>
+    class PecosPork
+    {
+        private bool bread = true;
+        /// <summary>
+        /// Include bread with the entree
+        /// </summary>
+        public bool Bread
+        {
+            get { return bread; }
+            set { bread = value; }
+        }
+
+        private bool pickle = true;
+        /// <summary>
+        /// Include a pickle with the entree
+        /// </summary>
+        public bool Pickle
+        {
+            get { return pickle; }
+            set { pickle = value; }
+        }
+
+        /// <summary>
+        /// The price of the pulled pork entree
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.88;
+            }
+        }
+        /// <summary>
+        /// Caloric content of the pulled pork
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 528;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for the preparation of the chicken
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!bread) instructions.Add("hold bread");
+                if (!pickle) instructions.Add("hold pickle");
+
+                return instructions;
+            }
+        }
+    }
+}
