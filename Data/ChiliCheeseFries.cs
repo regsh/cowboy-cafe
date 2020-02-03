c7e160164b116f252c92f493725eb2fa02162626
rshale <rshale@F1092-09-2097>
2020-02-03 16:48:47 -0600

    Chili cheese fries implemented

---
+++ Data/ChiliCheeseFries.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class ChiliCheeseFries : Side
+    {
+        public override double Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 3.99;
+                    case Size.Medium:
+                        return 2.99;
+                    case Size.Small:
+                        return 1.99;
+                    default:
+                        throw new NotImplementedException("unknown size");
+                }
+            }
+        }
+
+        public override uint Calories {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Large:
+                        return 610;
+                    case Size.Medium:
+                        return 524;
+                    case Size.Small:
+                        return 433;
+                    default:
+                        throw new NotImplementedException("Unknown size");
+                        
+                }
+            }
+        }
+    }
+}
