82f1cbb75c51215d69d7cae51070210b3508ed71
rshale <rshale@HFRR-3609-ACRH>
2020-02-13 20:22:58 -0600

    Added Drink abstract class, sodaflavor enum

---
+++ Data/Drink.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Drink.cs
+ * Author:Regan Hale
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    ///Abstract class to represent a generic drink at the Cowboy Cafe
+    /// </summary>
+    public abstract class Drink
+    {
+        /// <summary>
+        /// Represents the size of the drink, default of small
+        /// </summary>
+        public Size Size { get; set; } = Size.Small;
+
+        /// <summary>
+        /// Represents the price of the drink
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// Represents the caloric content of the drink
+        /// </summary>
+        public abstract uint Calories { get; }
+
+        /// <summary>
+        /// Whether the given drink contains ice
+        /// </summary>
+        public bool Ice { get; set; } = true;
+        /// <summary>
+        /// Special instructions for preparing the drink
+        /// </summary>
+        public abstract List<string> SpecialInstructions { get; }
+
+    }
+}
---
+++ Data/SodaFlavor.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * SodaFlavor.cs
+ * Author:Regan Hale
+ * Enum to represent the flavor options for soda Drink
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Enum representing soda flavors at the Cowboy Cafe
+    /// </summary>
+    enum SodaFlavor
+    {
+        CreamSoda,
+        OrangeSoda,
+        Sarsaparailla,
+        BirchBeer,
+        RootBeer
+    }
+}
