ce65c39313412413042cf4c173504e0d902b34a2
rshale <rshale@HFRR-3609-ACRH>
2020-02-13 20:28:10 -0600

    Added water class

---
+++ Data/Water.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Water.cs
+ * Author:Regan Hale
+ * Public class to represent an order of Water at the Cowboy Cafe
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Class to represent an order of water
+    /// </summary>
+    class Water : Drink
+    {
+        public override double Price => 0.12;
+
+        public override uint Calories => 0;
+
+        public bool Lemon { get; set; } = false;
+
+        public override List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                if (!Ice) instructions.Add("Hold Ice");
+                if (Lemon) instructions.Add("Add Lemon");
+                return instructions;
+            }
+
+        }
+    }
+}
