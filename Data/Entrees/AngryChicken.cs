232b1c0bfc60f951cdcf9daf9929fe0ffbbe34ab
rshale <rshale@HFRR-3609-ACRH>
2020-02-29 21:27:48 -0600

    Formatting for contents of order view complete

--- Data/Entrees/AngryChicken.cs
+++ Data/Entrees/AngryChicken.cs
@@ -62,6 +62,8 @@ namespace CowboyCafe.Data
             get
             {
                 var instructions = new List<string>();
+                instructions.Add("Sample instructions");
+
                 if (!bread) instructions.Add("hold bread");
                 if (!pickle) instructions.Add("hold pickle");
 
