cfb26d17f2fb35b42d99e63bd8220512126e527d
rshale <rshale@HFRR-3609-ACRH>
2020-03-04 20:39:03 -0600

    Finished chili customization screen

--- Data/Entrees/CowpokeChili.cs
+++ Data/Entrees/CowpokeChili.cs
@@ -32,8 +32,8 @@ namespace CowboyCafe.Data
         public bool Cheese
         {
             get { return cheese; }
-            set 
-            { 
+            set
+            {
                 cheese = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Cheese"));
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
@@ -47,7 +47,13 @@ namespace CowboyCafe.Data
         public bool SourCream
         {
             get { return sourCream; }
-            set { sourCream = value; }
+            set
+            {
+                sourCream = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SourCream"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+
+            }
         }
 
         private bool greenOnions = true;
@@ -57,19 +63,29 @@ namespace CowboyCafe.Data
         public bool GreenOnions
         {
             get { return greenOnions; }
-            set { greenOnions = value; }
+            set
+            {
+                greenOnions = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("GreenOnions"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
         }
 
         private bool tortillaStrips = true;
 
-        
+
         /// <summary>
         /// If the chili is topped with tortilla strips
         /// </summary>
         public bool TortillaStrips
         {
             get { return tortillaStrips; }
-            set { tortillaStrips = value; }
+            set
+            {
+                tortillaStrips = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TortillaStrips"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpecialInstructions"));
+            }
         }
 
         /// <summary>
