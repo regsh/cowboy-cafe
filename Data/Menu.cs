66c553db718137b705051f983a188762ce1d4acd
Regan <Regan@LAPTOP-UIL8RSD4>
2020-04-27 21:50:33 -0500

    Monday night work

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -5,6 +5,7 @@
  */
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CowboyCafe.Data
@@ -18,9 +19,9 @@ namespace CowboyCafe.Data
         {
             get => new string[]
             {
-            "Entree",
-            "Side",
-            "Drink"
+            "Entrees",
+            "Sides",
+            "Drinks"
             };
         }
 
@@ -86,6 +87,81 @@ namespace CowboyCafe.Data
                 completeMenu.Add(item);
             }
             return completeMenu;
+        }
+
+        public static IEnumerable<IOrderItem> CompleteSearch(IEnumerable<IOrderItem> items, string terms)
+        {
+            List<IOrderItem> results = new List<IOrderItem>();
+
+            if (terms == null) return CompleteMenu();
+            foreach(IOrderItem item in CompleteMenu())
+            {
+                if(item.Name.Contains(terms, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
+        }
+
+        public static IEnumerable<IOrderItem> FilterByCalories(IEnumerable<IOrderItem> items, uint? calorieMin, uint? calorieMax)
+        {
+            if (calorieMin == null && calorieMax == null) return items;
+            var results = new List<IOrderItem>();
+            if(calorieMin == null)
+            {
+                foreach(IOrderItem item in items)
+                {
+                    if (item.Calories <= calorieMax) results.Add(item);
+                }
+                return results;
+            }
+
+            if(calorieMax == null)
+            {
+                foreach(IOrderItem item in items)
+                {
+                    if (item.Calories >= calorieMin) results.Add(item);
+                }
+                return results;
+            }
+
+            foreach(IOrderItem item in items)
+            {
+                if(item.Calories >= calorieMin && item.Calories <= calorieMax)
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
+        }
+
+        public static IEnumerable<IOrderItem> FilterByPrice(IEnumerable<IOrderItem> items, double? priceMin, double? priceMax)
+        {
+            if (priceMin == null && priceMax == null) return items;
+            var results = new List<IOrderItem>();
+            if (priceMin == null)
+            {
+                foreach (IOrderItem item in items)
+                {
+                    if (item.Calories <= priceMax) results.Add(item);
+                }
+            }
+            else if (priceMax == null)
+            {
+                foreach (IOrderItem item in items)
+                {
+                    if (item.Calories >= priceMin) results.Add(item);
+                }
+            }
+            else foreach (IOrderItem item in items)
+            {
+                if (item.Calories >= priceMin && item.Calories <= priceMax)
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
         }
     }
 }
--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -1,34 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CowboyCafe.Data;
+﻿using CowboyCafe.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Website.Pages
 {
     public class IndexModel : PageModel
     {
-        [BindProperty (SupportsGet =true)]
+        [BindProperty(SupportsGet = true)]
         public string SearchTerms { get; set; }
 
-        [BindProperty (SupportsGet =true)]
-        public string[] ItemType { get; set; }
+        public string[] ItemType { get; set; } = new string[3] { "Entrees", "Sides", "Drinks" };
 
-        [BindProperty (SupportsGet =true)]
+        [BindProperty(SupportsGet = true)]
         public uint? CalorieMin { get; set; }
 
-        [BindProperty (SupportsGet =true)]
+        [BindProperty(SupportsGet = true)]
         public uint? CalorieMax { get; set; }
 
-        [BindProperty(SupportsGet =true)]
+        [BindProperty(SupportsGet = true)]
         public double? PriceMin { get; set; }
 
-        [BindProperty (SupportsGet =true)]
+        [BindProperty(SupportsGet = true)]
         public double? PriceMax { get; set; }
-        
+
+        public IEnumerable<IOrderItem> Entrees { get; protected set; }
+
+        public IEnumerable<IOrderItem> Sides { get; protected set; }
+
+        public IEnumerable<IOrderItem> Drinks { get; protected set; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -38,8 +42,26 @@ namespace Website.Pages
 
         public void OnGet()
         {
-            
+            ItemType = Request.Query[]
+            if (ItemType.Contains("Entrees"))
+            {
+                Entrees = Menu.CompleteSearch(Menu.Entrees(), SearchTerms);
+                Menu.FilterByCalories(Entrees, CalorieMin, CalorieMax);
+                Menu.FilterByPrice(Entrees, PriceMin, PriceMax);
+            }
+            if (ItemType.Contains("Sides"))
+            {
+                Sides = Menu.CompleteSearch(Menu.Sides(), SearchTerms);
+                Menu.FilterByCalories(Sides, CalorieMin, CalorieMax);
+                Menu.FilterByPrice(Sides, PriceMin, PriceMax);
+            }
 
+            if (ItemType.Contains("Drinks"))
+            {
+                Drinks = Menu.CompleteSearch(Menu.Drinks(), SearchTerms);
+                Menu.FilterByCalories(Drinks, CalorieMin, CalorieMax);
+                Menu.FilterByPrice(Drinks, PriceMin, PriceMax);
+            }
         }
     }
 }
